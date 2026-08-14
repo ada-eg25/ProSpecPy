4d48df44ab648a5c4d3b587db8a867fcb96075a4
ada-eg25 <eg25@ic.ac.uk>
2026-08-14 08:33:21 +0000

    Update arPLS baseline correction implementation

--- src/prospecpy/baseline.py
+++ src/prospecpy/baseline.py
@@ -440,6 +440,10 @@ def plot_baseline_corrected_data(
         ax.set_title(f"{sample_name}")
     ax.set_xlabel("wavenumber ($cm^{-1}$)")
     ax.set_ylabel("absorbance")
+
+    # FTIR convention: decreasing wavenumber from left to right
+    ax.invert_xaxis()
+
     ax.legend()
     if showplots:
         plt.show()
